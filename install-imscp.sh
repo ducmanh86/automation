150b31a85dcca3e1445ce791c18d38320824c41a
Tran Duc Manh <ducmanh86@users.noreply.github.com>
2016-02-24 03:27:31 +0700

    Update install-imscp.sh

--- install-imscp.sh
+++ install-imscp.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+echo "Please enter your hostname?"
+read hostname
+echo "Please enter your iMSCP version?"
+read version
+passwd
+hostname $hostname
+echo $hostname > /etc/hostname
+sed -i "s/masehub.com masehub/$hostname.masehub.com $hostname/g" /etc/hosts
 apt-get update && apt-get dist-upgrade
 apt-get install whiptail perl
 cd /usr/local/src
-wget https://github.com/i-MSCP/imscp/archive/1.2.12.tar.gz
-tar xzf 1.2.12.tar.gz
-cd imscp-1.2.12
+wget https://github.com/i-MSCP/imscp/archive/$version.tar.gz
+tar xzf $version.tar.gz
+cd imscp-$version
 perl ./imscp-autoinstall -d
